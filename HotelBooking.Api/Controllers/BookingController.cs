0b775d094b4d71cf8fad738e12eb42d2e20bef78
Artem Bykov <>
2026-07-25 11:48:11 +0500

    Add booking controller

---
+++ HotelBooking.Api/Controllers/BookingController.cs
@@ -0,0 +1,45 @@
+using AutoMapper;
+using HotelBooking.Api.Models;
+using HotelBooking.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelBooking.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BookingController(
+    IBookingService bookingService,
+    IMapper mapper) : ControllerBase
+{
+    private readonly IBookingService bookingService = bookingService;
+    private readonly IMapper mapper = mapper;
+
+    [HttpGet]
+    public async Task<ActionResult<List<Booking>>> GetBookings()
+    {
+        var bookings = await bookingService.GetBookings();
+        return Ok(bookings);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Booking>> GetBooking(int id)
+    {
+        var booking = await bookingService.GetBookingById(id);
+
+        if (booking == null)
+            return NotFound();
+
+        return Ok(booking);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteBooking(int id)
+    {
+        var result = await bookingService.DeleteBooking(id);
+
+        if (!result)
+            return NotFound();
+
+        return NoContent();
+    }
+}
